38669eacde0e271309ec3392d8867064ebb14a5d
قصي <98464635+Qusai-Z@users.noreply.github.com>
2022-08-27 17:08:51 +0300

    Add files via upload
    Wrap()

--- main.dart
+++ main.dart
@@ -17,117 +17,34 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: Icon(Icons.menu)),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    color: Color.fromARGB(255, 5, 27, 226)),
-                width: 300,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Text(
-                    'Strawbarry Pravlova Recipe',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                width: 300,
-                child: Text(
-                  'Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova. Pavlova features a crisp crust and soft, light inside,topped with fruit and whipped cream',
-                  style: TextStyle(fontSize: 18),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                decoration:
-                    BoxDecoration(border: Border.all(color: Colors.black)),
-                width: 300,
-                height: 200,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        RatingBar.builder(
-                          itemSize: 30,
-                          initialRating: 3,
-                          minRating: 1,
-                          direction: Axis.horizontal,
-                          allowHalfRating: true,
-                          itemCount: 5,
-                          itemPadding: EdgeInsets.symmetric(horizontal: 0.0),
-                          itemBuilder: (context, _) => Icon(
-                            Icons.star,
-                            color: Colors.amber,
-                          ),
-                          onRatingUpdate: (rating) {
-                            print(rating);
-                          },
-                        ),
-                        SizedBox(
-                          width: 40,
-                        ),
-                        Text(
-                          '17 Reviews',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Column(
-                          children: [
-                            Icon(
-                              Icons.food_bank_rounded,
-                              size: 70,
-                            ),
-                            Text('Feed'),
-                            Text('2.4')
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Icon(
-                              Icons.spa,
-                              size: 70,
-                            ),
-                            Text('Feed'),
-                            Text('3.5')
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Icon(
-                              Icons.coffee,
-                              size: 70,
-                            ),
-                            Text('Feed'),
-                            Text('4.9')
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
+        body: Wrap(
+          // master widget
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              color: Colors.blue,
+              width: 90,
+              height: 90,
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              color: Colors.blue,
+              width: 90,
+              height: 90,
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              color: Colors.blue,
+              width: 90,
+              height: 90,
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              color: Colors.blue,
+              width: 90,
+              height: 90,
+            ),
+          ],
         ),
       ),
     );
