c69a67a85cbd3d09c62b4a0ccebe5a4b37ef2013
قصي <98464635+Qusai-Z@users.noreply.github.com>
2022-09-06 14:20:36 +0300

    Add files via upload
    ScrollController sc = new ScrollController;
    
    Listview:
    
    List.generate()

--- main.dart
+++ main.dart
@@ -8,6 +8,7 @@ import 'package:practice/sqlfile.dart';
 import 'package:practice/test.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:practice/One.dart';
+import 'package:flutter/src/widgets/scroll_controller.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,38 +22,82 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _Value = 20.0;
+  final ScrollController sc = new ScrollController();
+
+  @override
+  void initState() {
+    sc.addListener(
+      () {
+        print(sc.offset);
+        print('Max scroll :${sc.position.maxScrollExtent}');
+      },
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("SLIDER"),
+          title: Center(
+            child: Text("SLIDER"),
+          ),
+          backgroundColor: Colors.black,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: ListView(
+          controller:
+              sc, //very very very imporatant:  This line catch this changes
           children: [
-            Slider(
-              activeColor: Colors.yellow,
-              inactiveColor: Colors.orange,
-              value: _Value,
-              min: 0.0,
-              max: 100,
-              onChanged: (double value) {
-                setState(
-                  () {
-                    _Value = value;
-                    print(_Value);
-                  },
+            ElevatedButton(
+              onPressed: () {
+                sc.animateTo(
+                  sc.position.maxScrollExtent,
+                  duration: Duration(milliseconds: 600),
+                  curve: Curves.ease,
                 );
               },
+              child: Text(
+                'Go to payment',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
             ),
-            Text(
-              "${_Value}",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ...List.generate(
+              // these three dotted are important to remove error
+              20,
+              (index) => Container(
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadiusDirectional.circular(20),
+                  color: index.isEven ? Colors.purpleAccent : Colors.black,
+                ),
+                margin: EdgeInsets.all(10),
+                child: Center(
+                  child: Text(
+                    'BOX ${index}',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                sc.animateTo(
+                  sc.position.minScrollExtent,
+                  duration: Duration(milliseconds: 600),
+                  curve: Curves.ease,
+                );
+              },
+              child: Text(
+                'Go back',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
             ),
           ],
         ),
+        backgroundColor: Color.fromARGB(255, 94, 94, 94),
       ),
     );
   }
